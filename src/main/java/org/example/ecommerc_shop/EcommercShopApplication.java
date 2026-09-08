f91f757cfb63e6277cd866ff2a4640207ec5622f
tongcongthanh27 <thanhcong2716@gmail.com>
2026-09-08 23:16:19 +0700

    first commit

---
+++ src/main/java/org/example/ecommerc_shop/EcommercShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.ecommerc_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/ecommerc_shop/EcommercShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ecommerc_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
